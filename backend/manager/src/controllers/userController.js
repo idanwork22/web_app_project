9d327294f489a4c0ee3db8207b80f3808480bf57
Yoav Buchnik <yoavbu1003@gmail.com>
2023-10-01 20:49:21 +0300

    add users first routes

---
+++ backend/manager/src/controllers/userController.js
@@ -0,0 +1,39 @@
+import userService from "../services/userService";
+
+const getAllUsers = (req, res) => {
+  const data = userService.getAllUsers();
+  res.json(data);
+};
+
+const getUserById = (req, res) => {
+  const id = req.params.id;
+  const data = userService.getUserById(id);
+  res.json(data ? data : 'NotFound');
+};
+
+const createUser = (req, res) => {
+  const name = req.body.name;
+  const newUser = userService.createUser({name});
+  res.json(newUser);
+};
+
+const updateUser = (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  const updatedUser = userService.updateUser(id, data);
+  res.json(updatedUser);
+};
+
+const deleteUser = (req, res) => {
+  const id = req.params.id;
+  const deletedUser = userService.deleteUser(id);
+  res.json(deletedUser);
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- backend/manager/src/index.js
+++ backend/manager/src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 app.use(express.json()); // Middleware for parsing JSON
 app.use(express.urlencoded({ extended: true }));
 
 
+app.use('/users', userRoutes); // Routes
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); // Replace with your actual frontend URL
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
