59f4137cabf5438e4ff0f01757b30b0b791588af
Yoav Buchnik <yoavbu1003@gmail.com>
2023-10-04 16:57:50 +0300

    add users routes to s3 gate

--- backend/s3-gate/src/index.js
+++ backend/s3-gate/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import postsRoutes from './routes/postsRoutes';
+import usersRoutes from './routes/usersRouter';
 import config from './config/config';
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/posts', postsRoutes); // Routes
+app.use('/users', usersRoutes); // Routes
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); // Replace with your actual frontend URL
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
