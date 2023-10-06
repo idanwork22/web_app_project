e9f248ca4b7b38749e44aca50467e27c55473a9d
Yoav Buchnik <yoavbu1003@gmail.com>
2023-10-06 13:24:26 +0300

    Creating the skeleton of the posts routs && check all get function

---
+++ backend/data-gate/src/controllers/postController.js
@@ -0,0 +1,56 @@
+import postService from "../services/postService";
+
+const getAllPosts = (db) => async (req, res) => {
+  const data = await postService.getAllPosts(db);
+  res.json(data);
+};
+
+const getPostById = (db) => async (req, res) => {
+  const id = req.params.id;
+  const data = await postService.getPostById(db, id);
+  res.json(data);
+};
+
+const createPost = (db) => async (req, res) => {
+  const postData = {
+    postname: req.body.postname,
+  };
+
+  const isAllDataAvailable = Object.values(postData).every(Boolean);
+  res.json(
+    isAllDataAvailable
+      ? await postService.createPost(db, postData)
+      : { success: false, result: "Incomplete post data provided." }
+  );
+};
+
+const updatePost = (db) => async (req, res) => {
+  const id = req.params.id;
+  const postData = {
+    postname: req.body.postname,
+  };
+
+  const filteredPostData = Object.keys(postData).reduce((acc, key) => {
+    if (postData[key] !== undefined) {
+      acc[key] = postData[key];
+    }
+    return acc;
+  }, {});
+
+  res.json(await postService.updatePost(db, id, filteredPostData));
+};
+
+const deletePost = (db) => async (req, res) => {
+  const id = req.params.id;
+  const deletedPost = await postService.deletePost(db, id);
+  res.json(deletedPost);
+};
+
+
+module.exports = {
+  getAllPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost,
+};
--- backend/data-gate/src/index.js
+++ backend/data-gate/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/userRoutes";
 import config from "./config/config";
 import { connect } from "./db.js";
+import postRoutes from "./routes/postRoutes";
 
 const startServer = async () => {
   const db = await connect();
@@ -10,6 +11,8 @@ const startServer = async () => {
   app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
   app.use("/users", userRoutes(db)); // Routes
+  app.use("/posts", postRoutes(db)); // Routes
+
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080"); // Replace with your actual frontend URL
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
