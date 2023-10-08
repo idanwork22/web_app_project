b16bb3d32b1a79fa8230c79b52e71d1dd79414f2
Yoav Buchnik <yoavbu1003@gmail.com>
2023-10-08 07:17:17 +0300

    task for groups to do

--- backend/manager/src/routes/groupRouter.js
+++ backend/manager/src/routes/groupRouter.js
@@ -8,5 +8,6 @@ router.get("/:id", groupController.getGroupById);
 router.post("/", groupController.createGroup);
 router.put("/:id", groupController.updateGroup);
 router.delete("/:id", groupController.deleteGroup);
+//TODO: add routes for add and remove user from group
 
 export default router;
